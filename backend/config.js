95eaa649c1d40682462075a02d7342fc7af437d0
Adam Maurasse <apmaurasse@gmail.com>
2024-05-14 14:14:53 -0400

    Updated config to connect with jobly server.

--- backend/config.js
+++ backend/config.js
@@ -10,10 +10,16 @@ const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
 const PORT = process.env.PORT || 3001;
 
 // Use dev database, testing database, or via env var, production database
+// function getDatabaseUri() {
+//   return (process.env.NODE_ENV === "test")
+//       ? "jobly_test"
+//       : process.env.DATABASE_URL || "jobly";
+// } Updated function so the backend would load.
+
 function getDatabaseUri() {
   return (process.env.NODE_ENV === "test")
-      ? "jobly_test"
-      : process.env.DATABASE_URL || "jobly";
+      ? "postgresql:///jobly_test"
+      : process.env.DATABASE_URL || "postgresql:///jobly";
 }
 
 // Speed up bcrypt during tests, since the algorithm safety isn't being tested
